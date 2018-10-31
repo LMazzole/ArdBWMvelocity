8e959f4c6f51bdce512f552449b2158ef9913ae1
Luca Mazzoleni <lmazzole@hsr.ch>
2018-10-31 14:44:54 +0100

    init

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#define MYDEBUG
+#include "debug.h"
+#include "pindefine.h"
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);                // Set serial monitor baud rate
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
